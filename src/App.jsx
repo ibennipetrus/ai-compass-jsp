fb594a941ebc1ae87c210a8ada0e7be32e68c5d9
ibennipetrus <bennipetrus1@gmail.com>
2025-08-16 11:42:37 +0200

    Test commit: check webhook trigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Checkliste from "./pages/checkliste";
 import Beratung from "./pages/beratung";
