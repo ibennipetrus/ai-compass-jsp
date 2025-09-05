390138f16a4279a9772c83b2ee76b7cef7d47240
ibennipetrus <bennipetrus1@gmail.com>
2025-09-05 10:41:23 +0200

    Update project

--- src/pages/whitepaper.jsx
+++ src/pages/whitepaper.jsx
@@ -227,7 +227,7 @@ visible,
                       <button
                         key={idx}
                         onClick={openContact}
-                        className="inline-flex w-auto self-start items-center whitespace-normal bg-black text-white px-5 py-2 rounded transition hover:bg-green-600"
+                        className="inline-flex w-auto self-start items-start whitespace-normal bg-black text-white px-5 py-2 rounded transition hover:bg-green-600"
                       >
                         {IconComponent && (
                           <IconComponent className="text-white shrink-0 mr-2" />
@@ -244,7 +244,7 @@ visible,
                         href={btn.file.asset.url}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="inline-flex w-auto self-start items-center whitespace-normal bg-black text-white px-5 py-2 rounded transition hover:bg-green-600"
+                        className="inline-flex w-auto self-start items-start whitespace-normal bg-black text-white px-5 py-2 rounded transition hover:bg-green-600"
                         download={false}
                       >
                         {IconComponent && (
@@ -261,7 +261,7 @@ visible,
                       href={btn.link}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="inline-flex w-auto self-start items-center whitespace-normal bg-black text-white px-5 py-2 rounded transition hover:bg-green-600"
+                      className="inline-flex w-auto self-start items-start whitespace-normal bg-black text-white px-5 py-2 rounded transition hover:bg-green-600"
                     >
                       {IconComponent && (
                         <IconComponent className="text-white shrink-0 mr-2" />
