2439605281779a22a2dcb1415cc81aaec2cce13c
ibennipetrus <bennipetrus1@gmail.com>
2025-09-22 10:38:48 +0200

    Update project

--- src/pages/Readiness.jsx
+++ src/pages/Readiness.jsx
@@ -187,7 +187,7 @@ export default function ReadinessCheck() {
     <div className="readiness-container flex flex-col min-h-screen">
       <main className="readiness-main flex-1 p-6 overflow-auto">
         <header className="readiness-header">
-          <img src="/Logo.png" alt="Logo" style={{ height: 40 }} />
+          <img src="./Logo.png" alt="Logo" style={{ height: 40 }} />
           <div>
             <h1 className="text-black text-4xl md:text-5xl font-semibold">
               KI Readiness Quick Check
