b01f72a4f9b3b00d1e8d51c151bd9ea4478d259a
ibennipetrus <bennipetrus1@gmail.com>
2025-09-22 12:48:18 +0200

    Update project

--- netlify/functions/saveReadiness.js
+++ netlify/functions/saveReadiness.js
@@ -1,20 +1,28 @@
-import client from "../../sanityClient"; // client hier serverseitig, mit Token
+import client from "@sanity/client";
+
+const sanityClient = client({
+  projectId: "5z6b0ok7",
+  dataset: "production",
+  useCdn: false,
+  apiVersion: "2025-08-12",
+  token: process.env.SANITY_WRITE_TOKEN,
+});
 
 export async function handler(event, context) {
-  const { name, email, answers, score, persona } = JSON.parse(event.body);
-
-  const doc = {
-    _type: "readinessResult",
-    name,
-    email,
-    answers,
-    score,
-    persona,
-    createdAt: new Date().toISOString(),
-  };
+  const body = JSON.parse(event.body);
 
   try {
-    const result = await client.create(doc);
+    const doc = {
+      _type: "readinessResult",
+      name: body.name,
+      email: body.email,
+      answers: body.answers,
+      score: body.score,
+      persona: body.persona,
+      createdAt: new Date().toISOString(),
+    };
+
+    const result = await sanityClient.create(doc);
     return {
       statusCode: 200,
       body: JSON.stringify({ id: result._id }),
