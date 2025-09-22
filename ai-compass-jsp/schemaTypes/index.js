c64526b091dbd574625c69aa529eebe0d52e0000
ibennipetrus <bennipetrus1@gmail.com>
2025-09-22 13:22:08 +0200

    Update project

--- ai-compass-jsp/schemaTypes/index.js
+++ ai-compass-jsp/schemaTypes/index.js
@@ -2,6 +2,5 @@ import Countdown from './Countdown'
 import Homepage from './Homepage'
 import HomeChecklist from './HomeChecklist'
 import HomeWhitepaper from './HomeWhitepaper'
-import Readiness from './Readiness'
 
-export const schemaTypes = [Homepage, Countdown, HomeChecklist, HomeWhitepaper, Readiness]
+export const schemaTypes = [Homepage, Countdown, HomeChecklist, HomeWhitepaper]
